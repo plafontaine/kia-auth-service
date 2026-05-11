b84931476311443eaf284f0c80040645ac687361
plafontaine <studio.styling.coiffure@hotmail.com>
2026-05-11 15:12:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def tokens_valid(tokens):
 
 def get_vehicle_manager():
     return VehicleManager(
-        region="CA",
+        region="CA_KIA",
         brand=BRAND_KIA,
         username=os.environ.get("KIA_USERNAME"),
         password=os.environ.get("KIA_PASSWORD"),
