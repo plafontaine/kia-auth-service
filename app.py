19140d8bf40c5e83ecc70809d624b584a6ff4c4e
plafontaine <studio.styling.coiffure@hotmail.com>
2026-05-29 08:30:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from playwright.sync_api import sync_playwright
 import json
 import os
-os.environ["PLAYWRIGHT_BROWSERS_PATH"] = "0"
+os.environ["PLAYWRIGHT_BROWSERS_PATH"] = "/opt/render/.cache/ms-playwright"
 
 import traceback
 
